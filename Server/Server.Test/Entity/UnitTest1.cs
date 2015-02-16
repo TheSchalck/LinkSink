9069f4bf9c88a50fe859d7fd5e5618a237535db9
Steffen Schalck <sts@schultz.dk>
2015-02-16 22:44:26 +0100

    Added empty Unit test project

---
+++ Server/Server.Test/Entity/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Server.Test.Entity
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
