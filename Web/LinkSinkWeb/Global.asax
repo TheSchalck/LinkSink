7c488b78b42a416cc36ab5f3ddb40996b5a48633
Steffen Schalck <sts@schultz.dk>
2015-07-26 20:36:39 +0200

    Added new Web project

---
+++ Web/LinkSinkWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LinkSinkWeb.MvcApplication" Language="C#" %>
---
+++ Web/LinkSinkWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LinkSinkWeb.Startup))]
+namespace LinkSinkWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
